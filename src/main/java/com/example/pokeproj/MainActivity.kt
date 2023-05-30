8803e2f042d2dfb4c3933b1c8b0aa575a88edae4
Florian ROBERT <florian.robert1@etu.univ-nantes.fr>
2023-05-30 18:33:16 +0200

    remplissage des stats a partir de l'api 1

--- src/main/java/com/example/pokeproj/MainActivity.kt
+++ src/main/java/com/example/pokeproj/MainActivity.kt
@@ -1,10 +1,15 @@
-package com.example.testapi
+package com.example.pokeproj
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
+import com.example.pokeproj.PokeInfo
 import com.example.pokeproj.R
+import com.example.testapi.PokebuildApi
+import com.example.testapi.Pokemon
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Dispatchers
@@ -17,6 +22,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btnOpen)
+        // Set up the click listener for the button
+        button.setOnClickListener {
+            // Create an intent to open the second activity
+            val intent = Intent(this, PokeInfo::class.java)
+
+            // Start the new activity
+            startActivity(intent)
+        }
 
         val moshi = Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
